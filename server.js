0071dac79918a45dad202a417dabcfa9da7dbc1b
ramzi bouchekoua <ramzibouchekoua@gmail.com>
2023-02-03 18:45:52 +0100

    fix server.js

--- server.js
+++ server.js
@@ -49,7 +49,8 @@ app.use(compression());
 
 // enable cors
 app.use('*', cors());
-app.all('/*', function (req, res, next) {
+app.options('*', cors());
+app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
