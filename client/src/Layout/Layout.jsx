83c756f456b5b6aaf4f30d25d85e7f20237e264c
ramzi bouchekoua <ramzibouchekoua@gmail.com>
2023-10-19 13:18:23 +0100

    v1 prod

--- client/src/Layout/Layout.jsx
+++ client/src/Layout/Layout.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Layout} from 'antd';
-import Header from "./Navbar";
-import Footer from "./Footer";
-const {  Content } = Layout;
+import React from 'react';
+import { Layout } from 'antd';
+import Header from './Navbar';
+import Footer from './Footer';
+import CallUs from './Navbar/CallUs';
+const { Content } = Layout;
 
 const HomeLayout = ({ children }) => {
   return (
     <Layout className="layout">
-      <Header/>
-        
-      <Content >
-        {children}
-      </Content>
-      
+      <CallUs />
+      <Header />
+
+      <Content>{children}</Content>
+
       <Footer />
     </Layout>
-  )
-}
+  );
+};
 
-export default HomeLayout
+export default HomeLayout;
