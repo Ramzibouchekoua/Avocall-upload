976f50ce74b376fe20e895895da569cb168cc2dc
RamziBouchekoua <ramzibouchekoua@gmail.com>
2024-01-16 16:27:43 +0100

    ajouter domaine de compétence lien

--- client/src/containers/Dashboard/dashboardPro.jsx
+++ client/src/containers/Dashboard/dashboardPro.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { List } from 'antd';
-import { Link } from 'react-router-dom';
+
 const Dashboard = () => {
   const [list] = useState([
     {
