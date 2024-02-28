340bad0780c417eea75e91fa620e0891b895c66a
RamziBouchekoua <ramzibouchekoua@gmail.com>
2024-02-28 16:23:41 +0100

    secure textchat consultation

--- client/src/containers/Text-chat/Textchat.jsx
+++ client/src/containers/Text-chat/Textchat.jsx
@@ -5,6 +5,8 @@ import ReportModal from '../../components/ReportModal';
 import { PaperClipOutlined, SendOutlined } from '@ant-design/icons';
 import moment from 'moment';
 import 'moment/locale/ar-tn';
+import displayNotification from '../../components/displayNotification';
+import { Col, Row, Spin } from 'antd';
 const Textchat = ({ socket }) => {
   const [reportVisible, setReportVisible] = useState(false);
   const { id } = useParams();
@@ -14,15 +16,68 @@ const Textchat = ({ socket }) => {
   const [theConsultation, setTheConsultation] = useState({});
   const messageRef = useRef();
   const [userId, setUserId] = useState('');
+  const [userOwner, setUserOwner] = useState(null);
+  const [statusOfPage, setStatusOfPage] = useState('Loading');
   useEffect(() => {
-    axios
-      .get(process.env.REACT_APP_API_URL + `/api/user/consultation/${chatroomId}`, {
-        headers: {
-          'x-auth-token': localStorage.getItem('auth-token'),
-        },
-      })
-      .then((res) => setTheConsultation(res.data[0]));
+    fetchData();
   }, []);
+  const authToken = localStorage.getItem('auth-token');
+
+  const fetchData = async () => {
+    try {
+      if (!authToken) {
+        // Redirect to sign-in page if auth token is not present
+        window.location.href = '/sign-in';
+      } else {
+        const userResponse = await axios.get(process.env.REACT_APP_API_URL + `/api/user/`, {
+          headers: {
+            'x-auth-token': authToken,
+          },
+        });
+        const owner = userResponse.data;
+        setUserOwner(owner);
+        console.log('azz s', userResponse.data._id);
+        if (owner) {
+          try {
+            const consultationResponse = await axios.get(
+              process.env.REACT_APP_API_URL + `/api/user/consultation/${chatroomId}`,
+              {
+                headers: {
+                  'x-auth-token': authToken,
+                },
+              }
+            );
+
+            if (consultationResponse.data[0]) {
+              const consultationUserId = consultationResponse.data[0].userId;
+              const userRoleId = userResponse.data.role;
+              const userId = userResponse.data._id;
+
+              if (userRoleId === 'ADMIN' || consultationUserId === userId) {
+                setTheConsultation(consultationResponse.data[0]);
+                setStatusOfPage('Allowed');
+              } else {
+                setStatusOfPage('Not Allowed');
+                displayNotification('error', 'خطأ', 'لا يسمح لك برؤية هذه الصفحة');
+              }
+            } else {
+              setStatusOfPage('Not Allowed');
+            }
+          } catch (error) {
+            // Handle error from consultation response
+            console.error('Error fetching consultation:', error);
+            setStatusOfPage('Not Allowed');
+          }
+        } else {
+          // Redirect to sign-in page if role is not present
+          window.location.href = '/dashboard';
+        }
+      }
+    } catch (error) {
+      // Handle errors here
+      console.error('Error fetching data:', error);
+    }
+  };
   const sendMessage = () => {
     if (socket) {
       socket.emit('chatroomMessage', {
@@ -84,101 +139,103 @@ const Textchat = ({ socket }) => {
     } catch (err) {}
   };
   return (
-    <div className="Text-chat">
-      <div className="Table">
-        <table className="az">
-          <tbody>
-            <tr>
-              <th>إستشارة كتابيّة</th>
-            </tr>
-            <tr>
-              <td>{theConsultation.field}</td>
-            </tr>
-            <tr>
-              <td>{theConsultation.title}</td>
-            </tr>
-          </tbody>
+    <>
+      {statusOfPage === 'Loading' ? (
+        <Spin tip="جاري..." />
+      ) : statusOfPage === 'Allowed' ? (
+        <div className="Text-chat">
+          <div className="Table">
+            <table className="az">
+              <tbody>
+                <tr>
+                  <th>إستشارة كتابيّة</th>
+                </tr>
+                <tr>
+                  <td>{theConsultation.field}</td>
+                </tr>
+                <tr>
+                  <td>{theConsultation.title}</td>
+                </tr>
+              </tbody>
 
-          <tbody>
-            <tr>
-              <th> التاريخ</th>
-            </tr>
-            <tr>
-              <td>{moment(theConsultation.date).format('LLLL')}</td>
-            </tr>
-          </tbody>
+              <tbody>
+                <tr>
+                  <th> التاريخ</th>
+                </tr>
+                <tr>
+                  <td>{moment(theConsultation.date).format('LLLL')}</td>
+                </tr>
+              </tbody>
 
-          <tbody>
-            <tr>
-              <th> الحالة</th>
-            </tr>
-            <tr>
-              <td> {theConsultation.isClosed || 'مفتوحة'}</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <th>تفاصيل الاستشارة</th>
-            </tr>
-            <tr>
-              <td>{theConsultation.description}</td>
-            </tr>
-          </tbody>
-          {/* <tbody>
-            <tr>
-              <div className="consultant">
-                <img src={User} alt="Consultant" />
-                <div>
-                  <th> المستشار القانوني</th>
-                  <tr> ابراهيم رمضان</tr>
+              <tbody>
+                <tr>
+                  <th> الحالة</th>
+                </tr>
+                <tr>
+                  <td>{theConsultation.isClosed || 'مفتوحة'}</td>
+                </tr>
+              </tbody>
+              <tbody>
+                <tr>
+                  <th>تفاصيل الاستشارة</th>
+                </tr>
+                <tr>
+                  <td>{theConsultation.description}</td>
+                </tr>
+              </tbody>
+            </table>
+            <Link to="/dashboard">
+              <button className="button-blue">أغلق الاستشارة</button>
+            </Link>
+            <Link to="/written-advice">
+              <button className="button-blue">تعديل الاستشارة</button>
+            </Link>
+            <button className="Connexion" onClick={() => setReportVisible(true)}>
+              مشكلة في الاستشارة ؟
+            </button>
+          </div>
+          <div className="chat-box-section">
+            <div className="chatroom-content">
+              {messages.map((message, i) => (
+                <div key={i} className="message">
+                  <span className={userId === message.user ? 'ownMessage' : 'otherMessage'}>{message.name}:</span>{' '}
+                  <p className="message-content"> {message.message}</p>
                 </div>
-              </div>
-            </tr>
-          </tbody> */}
-        </table>
-        <Link to="/dashboard">
-          <button className="button-blue">أغلق الاستشارة</button>
-        </Link>
-        <Link to="/written-advice">
-          <button className="button-blue">تعديل الاستشارة</button>
-        </Link>
-        <button className="Connexion " onClick={() => setReportVisible(true)}>
-          مشكلة في الاستشارة ؟
-        </button>
-      </div>
-      <div className="chat-box-section">
-        <div className="chatroom-content">
-          {messages.map((message, i) => (
-            <div key={i} className="message">
-              <span className={userId === message.user ? 'ownMessage' : 'otherMessage'}>{message.name}:</span>{' '}
-              <p className="message-content"> {message.message}</p>
+              ))}
             </div>
-          ))}
-        </div>
-        <div className="chatroom-actions">
-          <div className="message-field">
-            <input type="text" name="message" placeholder="Say something" ref={messageRef} />
+            <div className="chatroom-actions">
+              <div className="message-field">
+                <input type="text" name="message" placeholder="Say something" ref={messageRef} />
+              </div>
+              <div className="action-btn">
+                <button className="join upload" onClick={sendMessage}>
+                  <PaperClipOutlined />
+                </button>
+                <button className="join send" onClick={sendMessage}>
+                  <SendOutlined />
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="action-btn">
-            <button className="join upload" onClick={sendMessage}>
-              <PaperClipOutlined />
-            </button>
-            <button className="join send" onClick={sendMessage}>
-              <SendOutlined />
-            </button>
-          </div>
+          <ReportModal
+            visible={reportVisible}
+            handleCancel={() => {
+              setReportVisible(false);
+            }}
+            handleOk={() => {
+              setReportVisible(false);
+            }}
+          />
         </div>
-      </div>
-      <ReportModal
-        visible={reportVisible}
-        handleCancel={() => {
-          setReportVisible(false);
-        }}
-        handleOk={() => {
-          setReportVisible(false);
-        }}
-      />
-    </div>
+      ) : (
+        <Row justify="center" align="middle">
+          <Col>
+            <h1>غير مسموح</h1>
+            <p>آسف، أنت لست صاحب هذه الاستشارة، شكرًا لك</p>
+          </Col>
+        </Row>
+      )}
+    </>
   );
 };
 
