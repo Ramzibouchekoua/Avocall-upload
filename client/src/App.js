902e280fd70a6ee78c19c3822e6fde39ede7e02a
RamziBouchekoua <ramzibouchekoua@gmail.com>
2024-02-29 14:19:30 +0100

    modify navbar mobile for wallet and add contact us page in many fields

--- client/src/App.js
+++ client/src/App.js
@@ -131,7 +131,7 @@ export default function App() {
                   <PrivateAuth exact path="/sign-up" component={Signup} />
                   <PrivateAuth exact path="/sign-up-pro" component={SignupPro} />
                   <PrivateAuth exact path="/sign-in" component={Signin} />
-                  <PrivateAuth exact path="/contact" component={ContactUs} />
+                  <Route exact path="/contact" component={ContactUs} />
                   <Route exact path="/Wallet" component={Wallet} />
                   <Route exact path="/vid-page/:id" component={VidPage} />
                   <Route exact path="/text-chat/:id" render={() => <Textchat socket={socket} />} />
--- client/src/Layout/Navbar/mobileNavbar.jsx
+++ client/src/Layout/Navbar/mobileNavbar.jsx
@@ -8,7 +8,7 @@ import React from 'react';
 import { Layout, Menu, Dropdown } from 'antd';
 import { Link, useHistory } from 'react-router-dom';
 import { MenuOutlined } from '@ant-design/icons';
-import _ from "lodash"
+import _ from 'lodash';
 
 const { Header } = Layout;
 
@@ -20,63 +20,79 @@ const MobileNavbar = ({ user = {}, logout }) => {
   const login = () => {
     history.push('/sign-in');
   };
-  const menu = (
-    !_.isEmpty(user) ?
-      user.role === "USER" ?
-        //User Menu
-        <Menu>
-          <Menu.Item key="1">
-            <Link to="/dashboard">الرئيسية</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-           <a href="https://avocall.tn/bibliotheque-juridique/" >المكتبة القانونية</a>
-          </Menu.Item>
-
-          <Menu.Item key="3" className="signin" onClick={logout}>
-            الخروج
-          </Menu.Item>
-        </Menu>
-        //Pro Menu
-        : <Menu>
-
-          <Menu.Item key="1">
-            <Link to="/dashboardPro">جدول المواعيد</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <a href="https://calendly.com/app/scheduled_events/user/me" target="_blank" rel="noopener noreferrer">توقيت العمل</a>
-          </Menu.Item>
-          <Menu.Item key="3">
-           <a href="https://avocall.tn/bibliotheque-juridique/" >المكتبة القانونية</a>
-          </Menu.Item>
-
-          <Menu.Item key="4" className="signin" onClick={logout}>
-            الحروج
-  </Menu.Item>
-        </Menu>
-      //Default Menu
-      : <Menu>
+  const menu = !_.isEmpty(user) ? (
+    user.role === 'USER' ? (
+      //User Menu
+      <Menu>
         <Menu.Item key="1">
-          <Link to="/">الرئيسية</Link>
+          <Link to="/dashboard">الرئيسية</Link>
         </Menu.Item>
         <Menu.Item key="2">
-         <a href="https://avocall.tn/bibliotheque-juridique/" >المكتبة القانونية</a>
+          <a href="https://avocall.tn/bibliotheque-juridique/">المكتبة القانونية</a>
+        </Menu.Item>
+
+        <Menu.Item key="3" className="signin" onClick={logout}>
+          الخروج
+        </Menu.Item>
+      </Menu>
+    ) : (
+      //Pro Menu
+      <Menu>
+        {/* <Menu.Item key="1">
+          <Link to="/dashboardPro">جدول المواعيد</Link>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <a href="https://calendly.com/app/scheduled_events/user/me" target="_blank" rel="noopener noreferrer">
+            توقيت العمل
+          </a>
         </Menu.Item>
         <Menu.Item key="3">
-          <Link to="/sign-up-pro">أنا محامي</Link>
+          <a href="https://avocall.tn/bibliotheque-juridique/">المكتبة القانونية</a>
+        </Menu.Item> */}
+        <Menu.Item key="1">
+          <Link to="/DashboardAdmin">لوحة القيادة</Link>
         </Menu.Item>
-        <Menu.Item key="4" className="signup" onClick={register}>
-          التسجيل
-      </Menu.Item>
-        <Menu.Item key="5" className="signin" onClick={login}>
-          الدخول
-      </Menu.Item>
+        <Menu.Item key="4" className="signin" onClick={logout}>
+          الحروج
+        </Menu.Item>
       </Menu>
+    )
+  ) : (
+    //Default Menu
+    <Menu>
+      <Menu.Item key="1">
+        <Link to="/">الرئيسية</Link>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <a href="https://avocall.tn/bibliotheque-juridique/">المكتبة القانونية</a>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <Link to="/sign-up-pro">أنا محامي</Link>
+      </Menu.Item>
+      <Menu.Item key="4" className="signup" onClick={register}>
+        التسجيل
+      </Menu.Item>
+      <Menu.Item key="5" className="signin" onClick={login}>
+        الدخول
+      </Menu.Item>
+    </Menu>
   );
   return (
     <Header className="header">
-      <Link to="/" >
-        <div className="logo" />
+      <Link to="/">
+        <div className="logo-mobile" />
       </Link>
+      {user.role === 'USER' &&
+        (user.wallet > 0 ? (
+          <Link to="/written-advice">
+            <span className="user-wallet-mobile">{user.wallet}</span>
+          </Link>
+        ) : (
+          <Link to="/checkout">
+            <span className="user-wallet-mobile-empty">{user.wallet}</span>
+          </Link>
+        ))}
+
       <Dropdown overlay={menu} trigger={['click']}>
         <div className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
           <MenuOutlined />
