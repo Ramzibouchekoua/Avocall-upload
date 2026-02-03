e3f05e6c18eb18f01fb6555565878ac7c64633a7
RamziBouchekoua <ramzibouchekoua@gmail.com>
2026-02-03 01:58:26 +0100

    cors

--- server.js
+++ server.js
@@ -47,8 +47,17 @@ app.use(mongoSanitize());
 app.use(compression());
 
 // enable cors
-app.use(cors());
-app.options('*', cors());
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === 'production'
+      ? ['https://avocall.com', 'https://www.avocall.com']
+      : ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 // api routes
 app.use('/api', routes);
